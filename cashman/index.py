23342b61a8cd88d0800edf8fb896bfaa3939a1eb
Bruno Krebs <bruno.krebs@auth0.com>
2018-02-27 08:43:30 -0300

    enhancing RESTful endpoint;

--- cashman/index.py
+++ cashman/index.py
@@ -1,8 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+incomes = [
+    {'description': 'salary', 'amount': 5000}
+]
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+
+@app.route('/incomes')
+def get_incomes():
+    return jsonify(incomes)
+
+
+@app.route('/incomes', methods=['POST'])
+def add_income():
+    incomes.append(request.get_json())
+    return '', 204
